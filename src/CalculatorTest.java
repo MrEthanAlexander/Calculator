2809580575f0216ba981c6b60794ee60f9ee04ae
tienn1998 <tienn@email.sc.edu>
2019-05-23 15:47:33 -0400

    done

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -9,10 +9,13 @@ class CalculatorTest {
 		int fact=Calculator.factorial(5);
 		assertEquals(120,fact,"5! is 120");
 	}
-	
+
 	public void testDivide() {
 		double divideeee=Calculator.divide(3, 69);
 		assertEquals(23,divideeee,"69/3 is 23");
 	}
-	
+	public void testMax() {
+		int maxNum = Calculator.max(5,8);
+		assertEqualsuals(1, maxNum, "it cshould return 1");
+	}
 }
