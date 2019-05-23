b01d57ca1ed7560d0c7dda020e0357a4d850852b
tienn1998 <tienn@email.sc.edu>
2019-05-23 16:02:31 -0400

    done

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -9,21 +9,17 @@ class CalculatorTest {
 		int fact=Calculator.factorial(5);
 		assertEquals(120,fact,"5! is 120");
 	}
-	
+
 	@Test
 	public void testDivide() {
 		double divideeee=Calculator.divide(3, 69);
 		assertEquals(23,divideeee,"69/3 is 23");
 	}
-	
+
 	@Test
 	public void testMax() {
 		int maxNum = Calculator.max(5,8);
-<<<<<<< HEAD
-		assertEquals(1, maxNum, "it cshould return 1");
-=======
-		assertEqualsuals(8, maxNum, "it cshould return 8");
->>>>>>> cfbf4aba85e7209f60ae513da674b25b19f9a4f4
+		assertEquals(8, maxNum, "it should return 8");
 	}
 	@Test
 	public void testFibNormal() {
