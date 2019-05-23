c9669938debb23412ca1743cc300dff78755e147
MrEthanAlexander <mrethanalexander@gmail.com>
2019-05-23 15:22:00 -0400

    added the first two thingies

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,17 @@
 //i love puppies!
 public class Calculator {
-
+	
+	int factorial(int num)
+	{
+		for(int i=num-1;i>0;i--)
+		{
+			num=num*i;
+		}
+		return num;
+	}
+	double divide(int divisor, int dividend)
+	{
+		double answer=divisor/dividend;
+		return answer;
+	}
 }
