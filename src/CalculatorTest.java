b9b0470678ae90c6ef1a5f299f024c27c0619099
MrEthanAlexander <mrethanalexander@gmail.com>
2019-05-23 15:56:17 -0400

    meowdy

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -9,11 +9,14 @@ class CalculatorTest {
 		int fact=Calculator.factorial(5);
 		assertEquals(120,fact,"5! is 120");
 	}
-
+	
+	@Test
 	public void testDivide() {
 		double divideeee=Calculator.divide(3, 69);
 		assertEquals(23,divideeee,"69/3 is 23");
 	}
+	
+	@Test
 	public void testMax() {
 		int maxNum = Calculator.max(5,8);
 		assertEquals(1, maxNum, "it cshould return 1");
